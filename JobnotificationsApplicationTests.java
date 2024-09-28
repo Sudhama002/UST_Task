48c777209660bca8d7469f687ec1eeaa0f5050b8
Sudhama002 <125346443+Sudhama002@users.noreply.github.com>
2024-09-28 11:42:11 +0530

    Add files via upload

---
+++ JobnotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobnotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
